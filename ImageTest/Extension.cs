883896394363934c08e4fabb3441988c436ad3b2
shiggityshaggs <shiggityshaggs@users.noreply.github.com>
2023-03-23 21:28:04 +0100

    Add project files.

---
+++ ImageTest/Extension.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace ImageTest
+{
+    public static class Extension
+    {
+        public static Stream AsStream(this byte[]? data) =>
+            data is { } ?
+            new MemoryStream(data) :
+            new MemoryStream();
+    }
+}
---
+++ ImageTest/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.IO;
+
+namespace ImageTest
+{
+    internal partial class ViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Stream? _myStream;
+
+        bool swapper = true;
+
+        [RelayCommand]
+        public void RC()
+        {
+            byte[] barr = File.ReadAllBytes($"c:\\test{(swapper ? 1 : 2)}.bmp");
+            MyStream = new MemoryStream(barr);
+            swapper = !swapper;
+        }
+    }
+}
